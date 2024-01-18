fe435afbeff0967710abe22684fe6f922972831b
Swadesh Kumar <swadesh0287@gmail.com>
2024-01-18 22:27:54 +0530

    Update PageLayout.jsx

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
 const PageLayout = () => {
@@ -7,6 +8,7 @@ const PageLayout = () => {
     <>
       <Navbar />
       <Outlet />
+      <Footer/>
     </>
   );
 };
