4ac4676921affffe7ea1c92713c611979049473f
Swadesh Kumar <swadesh0287@gmail.com>
2024-01-20 00:36:47 +0530

    Update Tips.jsx

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -2,7 +2,7 @@ const Tips = () => (
   <>
     <div className="border border-b">
     <h1 className="font-bold  text-5xl m-2 p-2 text-orange-500 text-center">Good to Know</h1>
-    <div className="flex justify-between m-5 p-3">
+    <div className="flex flex-wrap justify-around m-5 p-3 sm:flex-col">
     <div className="w-32 h-32 bg-white shadow-lg rounded-md font-bold border border-slate-950 text-center my-6 py-9 hover:scale-110 transition duration-50 cursor-pointer">Free Entry of Everyone</div>
     <div className="w-32 h-32 bg-white shadow-lg rounded-md font-bold border border-slate-950 text-center my-6 py-9 hover:scale-110 transition duration-50 cursor-pointer">Cameras not allowed</div>
     <div className="w-32 h-32 bg-white shadow-lg rounded-md font-bold border border-slate-950 text-center my-6 py-9 hover:scale-110 transition duration-50 cursor-pointer">Timings -	7:00 AM to 7:00 PM</div>
