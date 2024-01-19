a2866834f86d84e41cce9b0ed951865f93a45352
Swadesh Kumar <swadesh0287@gmail.com>
2024-01-19 17:31:47 +0530

    Updated MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Culture from "../components/Culture";
 import Gallery from "../components/Gallery";
+import VideoSection from "../components/VideoSection";
 import ImageSlider from "../components/ImageSlider";
 
 const MainPage = () => {
   return (
     <div>
       <ImageSlider />
+      <VideoSection/>
       <Culture />
       <Gallery />
     </div>
