ed9c529d31047dfc29be8bd8799adbd2b865d70e
Chaitanya Agarwal <chaipanch98@gmail.com>
2024-01-21 20:31:26 +0530

    Tips pushed

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -12,10 +12,10 @@ const Tips = () => (
       <h1 className="font-bold  text-5xl mb-10 text-orange-500 text-center">
         Good to Know
       </h1>
-      <div className="grid grid-flow-col gap-6 md:px-16">
+      <div className="grid grid-cols-2 md:grid-flow-col gap-6 px-16">
         {tipsData.map((tip) => {
           return (
-            <div className=" text-xl bg-white shadow-lg rounded-md font-bold border border-slate-950 text-center my-6 py-9 hover:scale-110 transition duration-50 cursor-pointer">
+            <div className=" text-xl bg-white shadow-lg rounded-md font-bold border border-slate-950 text-center my-6 py-9 px-2 hover:scale-110 transition duration-50 cursor-pointer">
               {tip}
             </div>
           );
