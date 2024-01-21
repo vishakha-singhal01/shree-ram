f51328942bb0ec6fea62026b928228159a932aa8
Swadesh Kumar <swadesh0287@gmail.com>
2024-01-21 20:47:21 +0530

    Updates index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 
   return (
     <>
-      <header className="flex max-w-9xl bg-orange-500 text-black font-serif shadow-lg justify-between items-center">
+      <header className="sticky top-0 z-50 flex max-w-9xl bg-orange-500 text-black font-serif shadow-lg justify-between items-center">
         <div className="w-20 h-22">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAqVsfXnzIqrjkKt9OWW2t0H67XVQab2gfvw&usqp=CAU"
