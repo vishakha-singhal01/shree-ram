88a3785048bb53d5f27f76953ecb91a7c8968029
Chaitanya Agarwal <chaipanch98@gmail.com>
2024-01-21 23:49:52 +0530

    hero-fix

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,11 +1,12 @@
 // ImageSlider.js
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import RamMandirPic from "../../public/images/RamMandirHome.jpg";
 
 const ImageSlider = () => {
   const images = [
     // "https://e1.pxfuel.com/desktop-wallpaper/1002/384/desktop-wallpaper-ram-darbar.jpg",
 
-    "https://im.rediff.com/news/2020/aug/05modi-ram1.jpg",
+    RamMandirPic,
     // 'https://im.rediff.com/news/2020/aug/05modi-ram2.jpg',
     // "https://swarajya.gumlet.io/swarajya/2020-08/bfc54327-1c31-4480-9e5b-fe589faff092/EekLzZ4UMAACUH5.jpg",
   ];
@@ -24,7 +25,7 @@ const ImageSlider = () => {
 
   return (
     <div className="w-full">
-      <div className="relative overflow-hidden w-full md:h-[90vh]">
+      <div className="relative overflow-hidden w-full md:h-[91vh]">
         {images.map((image, index) => (
           <img
             key={index}
@@ -37,6 +38,12 @@ const ImageSlider = () => {
             }`}
           />
         ))}
+        <div className="absolute top-0 left-0 w-full h-3/4 flex items-center text-2xl md:text-5xl text-white justify-center">
+          <p className="leading-relaxed text-center font-thin">
+            रामचंद्र के चरणों में मेरा सब कुछ समर्पित है,
+            <br /> उनकी कृपा से ही मैं आत्मा को पाता हूँ।
+          </p>
+        </div>
       </div>
     </div>
   );
