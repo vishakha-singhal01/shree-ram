4bccb83d6a2fc8f7c900a7de209b35cae85f5a69
Chaitanya Agarwal <chaipanch98@gmail.com>
2024-01-21 23:27:19 +0530

    responsive-fix

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,11 +1,11 @@
 // ImageSlider.js
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState, useEffect, useRef } from "react";
 
 const ImageSlider = () => {
   const images = [
     // "https://e1.pxfuel.com/desktop-wallpaper/1002/384/desktop-wallpaper-ram-darbar.jpg",
 
-    'https://im.rediff.com/news/2020/aug/05modi-ram1.jpg',
+    "https://im.rediff.com/news/2020/aug/05modi-ram1.jpg",
     // 'https://im.rediff.com/news/2020/aug/05modi-ram2.jpg',
     // "https://swarajya.gumlet.io/swarajya/2020-08/bfc54327-1c31-4480-9e5b-fe589faff092/EekLzZ4UMAACUH5.jpg",
   ];
@@ -24,13 +24,13 @@ const ImageSlider = () => {
 
   return (
     <div className="w-full">
-      <div className="relative overflow-hidden h-[88vh]">
+      <div className="relative overflow-hidden w-full md:h-[90vh]">
         {images.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={`Slide ${index + 1}`}
-            className={`absolute top-0 left-0 w-full h-full object-cover transition-transform duration-500 ease-in-out transform ${
+            className={`w-full h-full object-fit transition-transform duration-500 ease-in-out transform ${
               index === currentSlide
                 ? "opacity-100 scale-100"
                 : "opacity-0 scale-100"
